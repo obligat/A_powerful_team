7cebf7a2f659d247701bdcf98a1f983694fa0783
myc1995 <875940302@qq.com>
2016-10-11 22:50:13 +0800

    myc add new component

--- public/src/components/Login.js
+++ public/src/components/Login.js
@@ -1,11 +1,48 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router';
 
 class Login extends Component {
   render() {
 
     return (
-        <div>
-          登录
+        <div className="container">
+
+          <div className="form-group  container">
+            <label className="col-xs-1 col-form-label">Username</label>
+            <div className="col-xs-2">
+              <input className="form-control"
+                     id="username-input"
+                     type="text"
+                     placeholder="用户名"/>
+            </div>
+          </div>
+
+          <div className="form-group  container">
+            <label className="col-xs-1 col-form-label">Password</label>
+            <div className="col-xs-2">
+              <input className="form-control"
+                     id="password-input"
+                     type="text"
+                     placeholder="密码"/>
+            </div>
+          </div>
+
+          <Link to="/">
+            <button id="cancelBulid"
+                    type="button"
+                    className="btn">
+              <strong>登录</strong>
+            </button>
+          </Link>
+
+          <Link to="/register">
+            <button id="cancelBulid"
+                    type="button"
+                    className="btn">
+              <strong>注册</strong>
+            </button>
+          </Link>
+
         </div>
     );
   }
