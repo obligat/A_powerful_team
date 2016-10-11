2c1746a138244dadb825ca554f988d4103408b21
myc1995 <875940302@qq.com>
2016-10-11 22:51:44 +0800

    myc add new path

--- public/src/index.js
+++ public/src/index.js
@@ -6,6 +6,7 @@ import App from './components/App';
 import Main from './components/Main';
 import Register from './container/Register';
 import Login from './components/Login';
+import NewActivity from './components/NewActivity';
 import activityList from './reducers';
 import ActivityDetail from './container/ActivityDetail';
 import {createStore, applyMiddleware} from 'redux';
@@ -29,6 +30,7 @@ render(
         <Route path='/register' components={Register}/>
         <Route path='/login' components={Login}/>
         <Route path="/activityDetail/:id" components={ActivityDetail}/>
+        <Route path="/newActivity" components={NewActivity}/>
       </Route>
     </Router>
   </Provider>,
