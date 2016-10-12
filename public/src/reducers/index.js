288f61807e5dc0ce86a1adbda6580f96c5724992
myc1995 <875940302@qq.com>
2016-10-12 23:14:39 +0800

    myc update

--- public/src/reducers/index.js
+++ public/src/reducers/index.js
@@ -2,11 +2,14 @@ import {combineReducers} from 'redux';
 import activities from './activities';
 import activityDetail from './activityDetail';
 import showRegisterMessage from './showRegisterMessage';
+import addNewActivity from './addNewActivity';
+
 
 const resultList = combineReducers({
   activities,
   activityDetail,
-  showRegisterMessage
+  showRegisterMessage,
+  addNewActivity,
 });
 
 export default resultList;
