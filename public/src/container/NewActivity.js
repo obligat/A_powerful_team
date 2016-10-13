dc981096a1a7162a2be003b7efdc12a861d8dcb7
myc1995 <875940302@qq.com>
2016-10-13 14:35:46 +0800

    myc import withRouter

--- public/src/container/NewActivity.js
+++ public/src/container/NewActivity.js
@@ -1,13 +1,11 @@
-/**
- * Created by myc on 10/12/16.
- */
 import {connect} from 'react-redux';
+import {withRouter} from 'react-router';
 import {reqAddActivity} from '../actions/newActivity';
 import NewActivity from '../components/NewActivity';
 
 const mapStateToProps = (state)=> {
   return {
-    addNewActivity: state.addNewActivity
+    addNewActivityResult: state.addNewActivityResult
   };
 };
 
@@ -16,7 +14,7 @@ const mapDispatchToProps = (dispatch)=> {
     reqAddActivity: (activityName, activityTime, activityLocation, instruction)=> {
       dispatch(reqAddActivity(activityName, activityTime, activityLocation, instruction));
     }
-  };
+  }
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(NewActivity);
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(NewActivity));
