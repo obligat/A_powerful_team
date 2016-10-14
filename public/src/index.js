d6cd4d4e9008e5631499169b9d1820051d3bf831
dujinqiao <duzi1996@qq.com>
2016-10-14 12:22:51 +0800

    djq - modify src/index.js

--- public/src/index.js
+++ public/src/index.js
@@ -5,9 +5,10 @@ import createLogger from 'redux-logger'
 import {Router, Route, browserHistory, IndexRoute, Redirect} from 'react-router';
 import App from './components/App';
 import Main from './components/Main';
+import UserCenter from './components/UserCenter';
 import Register from './container/Register';
 import Login from './container/Login';
-import NewActivity from './components/NewActivity';
+import NewActivity from './container/NewActivity';
 import activityList from './reducers';
 import ActivityDetail from './container/ActivityDetail';
 import {createStore, applyMiddleware} from 'redux';
@@ -15,8 +16,8 @@ import {Provider} from 'react-redux';
 import activitiesRequestMiddleware from './middlewares/activitiesRequestMiddleware';
 
 const store = createStore(
-  activityList,
-  applyMiddleware(activitiesRequestMiddleware, thunkMiddleware,createLogger())
+    activityList,
+    applyMiddleware(activitiesRequestMiddleware, thunkMiddleware, createLogger())
 );
 
 store.dispatch({
@@ -24,16 +25,18 @@ store.dispatch({
 });
 
 render(
-  <Provider store={store}>
-    <Router history={browserHistory}>
-      <Route path='/' components={App}>
-        <IndexRoute components={Main}/>
-        <Route path='/register' components={Register}/>
-        <Route path='/login' components={Login}/>
-        <Route path="/activityDetail/:id" components={ActivityDetail}/>
-        <Route path="/newActivity" components={NewActivity}/>
-      </Route>
-    </Router>
-  </Provider>,
-  document.getElementById('app')
+    <Provider store={store}>
+      <Router history={browserHistory}>
+        <Route path='/' components={App}>
+          <IndexRoute components={Login}/>
+          <Route path='/register' components={Register}/>
+          <Route path='/main' components={Main}/>
+          <Route path='/userCenter/:username' components={UserCenter}/>
+          <Route path='/login' components={Login}/>
+          <Route path="/activityDetail/:id" components={ActivityDetail}/>
+          <Route path="/newActivity" components={NewActivity}/>
+        </Route>
+      </Router>
+    </Provider>,
+    document.getElementById('app')
 );
