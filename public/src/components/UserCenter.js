17e057ce2629e6eca5b0704d496ba6088e88a994
dujinqiao <duzi1996@qq.com>
2016-10-14 12:20:17 +0800

    djq - add UserCenter container

--- public/src/components/UserCenter.js
+++ public/src/components/UserCenter.js
@@ -1,3 +1,40 @@
-/**
- * Created by dujinqiao on 16-10-14.
- */
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {reqUserInfo} from '../actions/showUserInfo';
+
+class UserCenter extends Component {
+
+  componentDidMount() {
+    let {username}=this.props.params;
+    this.props.reqUserInfo(username);
+  }
+
+  render() {
+    const {username, communityName, schoolName, logo, description, email, tel}=this.props.showUserInfo;
+    return (
+        <div>
+          <div>{communityName}</div>
+          <div>{schoolName}</div>
+          <div>
+            <img src={logo} alt='community logo '></img>
+          </div>
+          <div>{username}</div>
+          <div>{email}</div>
+          <div>{tel}</div>
+          <div>{description}</div>
+        </div>
+    )
+  }
+}
+
+const mapStateToProps = state=>state;
+const mapDispatchToProps = (dispatch)=> {
+  return {
+    reqUserInfo: (username)=> {
+      dispatch(reqUserInfo(username));
+    }
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserCenter);
+
