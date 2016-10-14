5d650051db903ca5d14a6d32e6e992f4d6ef126c
dujinqiao <duzi1996@qq.com>
2016-10-14 12:19:29 +0800

    djq - modify index reducer

--- public/src/reducers/index.js
+++ public/src/reducers/index.js
@@ -3,15 +3,18 @@ import activities from './activities';
 import activityDetail from './activityDetail';
 import showRegisterMessage from './showRegisterMessage';
 import addNewActivity from './addNewActivity';
-
 import showLoginMessage from './showLoginMessage';
+import showUserInfo from './showUserInfo';
+import reqUserNameResult from './reqUserNameResult';
 
 const resultList = combineReducers({
   activities,
   activityDetail,
   showRegisterMessage,
   showLoginMessage,
-  addNewActivity
+  addNewActivity,
+  showUserInfo,
+  reqUserNameResult
 });
 
 export default resultList;
