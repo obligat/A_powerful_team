12b418e9ed5965d209f24df37ce64c00d18ae55a
dujinqiao <duzi1996@qq.com>
2016-10-14 12:13:31 +0800

    djq - add showUserInfo action

--- public/src/actions/showUserInfo.js
+++ public/src/actions/showUserInfo.js
@@ -1,18 +1,23 @@
 import request from 'superagent';
 
 export const GET_USER_INFO = 'GET_USER_INFO';
+export const GET_USER_NAME = 'GET_USER_NAME';
 
 export const getUserInfo = (info)=>({
   type: GET_USER_INFO,
   info
 });
 
+export const getUserName = (username)=>({
+  type: GET_USER_NAME,
+  username
+});
+
 export const reqUserInfo = (username)=> {
 
   return (dispatch)=> {
     request
-        .get('/community/userInfo')
-        .query({username: username})
+        .get(`/community/userCenter/${username}`)
         .end((err, res)=> {
           if (err) {
             throw err;
@@ -23,19 +28,16 @@ export const reqUserInfo = (username)=> {
   }
 };
 
-export const confirmUserInfo = ()=> {
+export const reqUserName = ()=> {
 
   return (dispatch)=> {
     request
-        .get('/community/confirmUserInfo')
+        .get('/community/reqUserName')
         .end((err, res)=> {
           if (err) {
             throw err;
           } else {
-            dispatch({
-              type: 'VALID_USER',
-              data: res.body
-            });
+            dispatch(getUserName(res.text));
           }
         });
   }
