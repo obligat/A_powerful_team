e0f0cd84bc26eee4cd42a9d44307f19934e0f55f
myc1995 <875940302@qq.com>
2016-10-16 18:30:25 +0800

    myc update foot

--- public/src/components/Foot.js
+++ public/src/components/Foot.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';// eslint-disable-line no-unused-vars
 class Foot extends Component {
   render() {
     return (
-      <div className="row">
+      <div className="col-xs-12">
         <h3 className="text-center">about us</h3>
       </div>
     );
