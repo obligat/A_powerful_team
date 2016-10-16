0419d685ad4be7dc3b0083b04b65831e813d4d5c
myc1995 <875940302@qq.com>
2016-10-16 18:29:40 +0800

    myc add creator

--- public/src/components/ActivityDetail.js
+++ public/src/components/ActivityDetail.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react';
+import React, {Component} from "react";
+
 
 export default class ActivityDetail extends Component {
 
@@ -6,34 +7,35 @@ export default class ActivityDetail extends Component {
     let {id} = this.props.params;
     this.props.requestActivityDetail(id);
   }
-
+  
   render() {
-    const {activityName, activityTime, activityLocation, instruction}=this.props.activityDetail;
+    const {creator, activityName, activityTime, activityLocation, instruction}=this.props.activityDetail;
     return (
-      <div>
         <div>
-          <div className="col-xs-4">
-            logo
+          <div>
+            <div className="col-xs-8">
+              <div>活动名称：{activityName}</div>
+              <div>举办者：{creator}</div>
+              <div>举办时间：{activityTime}</div>
+              <div>举办地点：{activityLocation}</div>
+            </div>
           </div>
           <div className="col-xs-8">
-            <div>活动名称：{activityName}</div>
-            <div>举办社团：</div>
-            <div>举办时间：{activityTime}</div>
-            <div>举办地点：{activityLocation}</div>
+            活动简介：{instruction}
           </div>
-        </div>
-        <div>
-          活动简介：{instruction}
-        </div>
-        <div>
-          评论：
-          <textarea className="form-control" rows="3" style={{width: '700px'}}></textarea>
-          <button className="btn btn-info">回复</button>
+
+          <div className="col-xs-8">
+            <div>
+              评论：
+            </div>
+            <div>
+              <textarea className="form-control" rows="3" style={{width: '700px'}}></textarea>
+              <button className="btn btn-info">回复</button>
+            </div>
+          </div>
 
         </div>
-      </div>
 
     );
   }
 }
-
