27c819c2ef80a33467d9ed1a0cb13a6e51fa47c5
myc1995 <875940302@qq.com>
2016-10-17 23:25:47 +0800

    myc add username in router

--- public/src/actions/newActivity.js
+++ public/src/actions/newActivity.js
@@ -1,7 +1,7 @@
 /**
  * Created by myc on 10/12/16.
  */
-import request from 'superagent';
+import request from "superagent";
 
 export const ADD_ACTIVITY = 'ADD_ACTIVITY';
 
@@ -10,16 +10,17 @@ export const addActivity = (errorMessage)=>({
   errorMessage
 });
 
-export const reqAddActivity = (activityName, activityTime, activityLocation, instruction)=> {
+export const reqAddActivity = (username, activityName, activityTime, activityLocation, instruction)=> {
   return (dispatch)=> {
     request
         .post('/activity/save')
         .type('form')
         .send({
-          activityName:activityName,
-          activityTime:activityTime,
-          activityLocation:activityLocation,
-          instruction:instruction
+          username: username,
+          activityName: activityName,
+          activityTime: activityTime,
+          activityLocation: activityLocation,
+          instruction: instruction
         })
         .end((err, res)=> {
           if (err) {
