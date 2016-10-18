b30ae8a54a313a8ffaf59d291b3ef3a9ad199932
dujinqiao <duzi1996@qq.com>
2016-10-18 17:04:58 +0800

    djq - fix login show state problem

--- public/src/components/Head.js
+++ public/src/components/Head.js
@@ -1,22 +1,48 @@
 import React, {Component} from 'react';// eslint-disable-line no-unused-vars
 import {Link} from 'react-router';
+import {connect} from 'react-redux';
 
 class Head extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      login: '登陆',
+      register: '注册'
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.reqUserNameResult) {
+      this.setState({
+        login: nextProps.reqUserNameResult,
+        register: ''
+      });
+    } else {
+      this.setState({
+        login: '登陆',
+        register: '注册'
+      });
+    }
+  }
+
   render() {
     return (
-      <div className="row head">
-        <div className="col-xs-4 col-md-1">
-          <div className="thumbnail">
-            <img src="images/logo.jpg" alt="iPet logo"></img>
+        <div className="row head">
+          <div className="col-xs-4 col-md-1">
+            <div className="thumbnail">
+              <img src="images/logo.jpg" alt="iPet logo"/>
+            </div>
+          </div>
+          <div className="col-xs-8 ">
+            <Link to="/main"><span>首页    </span></Link>
+            <Link to="/login"><span>{this.state.login}   </span></Link>
+            <Link to="/register"><span>{this.state.register}    </span></Link>
           </div>
         </div>
-        <div className="col-xs-8 ">
-          <Link to="/main"><span>首页    </span></Link>
-          <Link to="/login"><span>登陆    </span></Link>
-          <Link to="/register"><span>注册    </span></Link>
-        </div>
-      </div>
     );
   }
 }
-export default Head;
+
+const mapStateToProps = state=>state;
+export default connect(mapStateToProps)(Head);
